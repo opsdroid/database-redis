052ff05c496bdc580980ca7eb536b7a39ba7adea
arhix <arhixx@gmail.com>
2017-06-28 13:50:59 +0200

    redis database module

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# opsdroid-redis-database
-A redis database module for opsdroid
+# opsdroid database redis
+
+A database module for [opsdroid](https://github.com/opsdroid/opsdroid) to persist memory in a [redis database](https://redis.io/).
+
+## Requirements
+
+None.
+
+## Configuration
+
+```yaml
+databases:
+  mongo:
+    host:       "my host"     # (optional) default "localhost"
+    port:       "12345"       # (optional) default "6379"
+    database:   "7"           # (optional) default "0"
+```
+
+## License
+
+GNU General Public License Version 3 (GPLv3)
---
+++ redis.py
@@ -0,0 +1,50 @@
+import json
+import time
+import aioredis
+
+from datetime import date, datetime
+from opsdroid.database import Database
+
+
+class RedisDatabase(Database):
+    async def connect(self, opsdroid):
+        host = self.config["host"] if "host" in self.config else "localhost"
+        port = self.config["port"] if "port" in self.config else "6379"
+        database = self.config["database"] if "database" in self.config else 0
+        self.client = await aioredis.create_connection(
+            (host, port), db=database)
+
+    async def put(self, key, data):
+        data = self.convert_object_to_timestamp(data)
+        await self.client.execute('SET', key, json.dumps(data))
+
+    async def get(self, key):
+        data = await self.client.execute('GET', key)
+
+        if data:
+            return self.convert_timestamp_to_object(json.loads(data))
+
+        return None
+
+    @staticmethod
+    def convert_object_to_timestamp(data):
+        for k, value in data.items():
+            if isinstance(value, (datetime, date)):
+                value = '::'.join([
+                    type(value).__name__,
+                    '%d' % time.mktime(value.timetuple())
+                ])
+                data[k] = value
+        return data
+
+    @staticmethod
+    def convert_timestamp_to_object(data):
+        for k, value in data.items():
+            value_type = value.split('::', 1)[0]
+            timestamp = int(value.split('::', 1)[1])
+            if value_type == 'datetime':
+                value = datetime.fromtimestamp(timestamp)
+            elif value_type == 'date':
+                value = date.fromtimestamp(timestamp)
+            data[k] = value
+        return data
